b80a479bbd4b432d82e062340f9394c47f16e3e3
Egonss <83237308+Egonss@users.noreply.github.com>
2021-09-03 12:04:42 +0300

    Added more info in Products page in Django Admin

--- MountainPeak/store/admin.py
+++ MountainPeak/store/admin.py
@@ -1,9 +1,19 @@
+from django import forms
 from django.contrib import admin
 
 from .models import Category, Product, ProductImage
 
-admin.site.register(Product)
-admin.site.register(ProductImage)
+
 admin.site.register(Category)
 
-# Register your models here.
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'stock_count')
+
+
+class ProductImageAdmin(admin.ModelAdmin):
+    list_display = ('product', 'image')
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(ProductImage, ProductImageAdmin)
